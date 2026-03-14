aeba9c60fae457a3520a1768deb2b6250c2c4f2f
dimitarbeograd-QA <dmar91922@gmail.com>
2026-03-14 18:18:18 +0200

    Assert response type in surname test
    Added assertion for response type in surname test.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -36,19 +36,20 @@ suite('Functional Tests', function () {
     });
 
     // #3
-    test('Send {surname: "Colombo"}', function (done) {
-      chai
-        .request(server)
-        .keepOpen()
-        .put('/travellers')
-        .send({ surname: 'Colombo' })
-        .end(function (err, res) {
-          assert.equal(res.status, 200);
-          assert.equal(res.body.name, 'Cristoforo');
-          assert.equal(res.body.surname, 'Colombo');
-          done();
-        });
+test('Send {surname: "Colombo"}', function (done) {
+  chai
+    .request(server)
+    .keepOpen()
+    .put('/travellers')
+    .send({ surname: 'Colombo' })
+    .end(function (err, res) {
+      assert.equal(res.status, 200);
+      assert.equal(res.type, 'application/json');
+      assert.equal(res.body.name, 'Cristoforo');
+      assert.equal(res.body.surname, 'Colombo');
+      done();
     });
+});
 
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
