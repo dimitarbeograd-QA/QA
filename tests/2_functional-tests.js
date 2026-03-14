dffbb52998c413b2abb93aceed2e15f561d0fd9b
dimitarbeograd-QA <dmar91922@gmail.com>
2026-03-14 19:39:43 +0200

    Update Browser site URL for functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -68,7 +68,7 @@ suite('Functional Tests', function () {
   });
 
   const Browser = require('zombie');
-  Browser.site = 'http://0.0.0.0:3000';
+  Browser.site = 'https://qa-w0ba.onrender.com';
 
   suite('Functional Tests with Zombie.js', function () {
     this.timeout(5000);
