24f58bbe122623819dc48a957ca9458530403803
dimitarbeograd-QA <dmar91922@gmail.com>
2026-04-13 07:05:52 +0000

    test

--- server.js
+++ server.js
@@ -1,119 +1,136 @@
-'use strict'
+'use strict';
+require('dotenv').config();
 const express = require('express');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
+const { ObjectId } = require('mongodb');
+const LocalStrategy = require('passport-local');
+
 const app = express();
 
-const cors = require('cors');
-const runner = require('./test-runner');
+// -----------------------------
+// 1. Настройка на Pug (ВАЖНО)
+// -----------------------------
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
+// -----------------------------
+// Middleware
+// -----------------------------
+fccTesting(app);
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-})
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
 
-app.use(express.static(__dirname + '/public'));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/hello', function (req, res) {
-  const name = req.query.name || 'Guest';
-  res.type('txt').send('hello ' + name);
-})
+// -----------------------------
+// 2. Основен маршрут (за теста)
+// -----------------------------
+app.route('/').get((req, res) => {
+  res.render('index');   // FreeCodeCamp ИСКА ТОЧНО ТОВА
+});
 
-const travellers = function (req, res) {
-  let data = {};
-  if (req.body && req.body.surname) {
-    switch (req.body.surname.toLowerCase()) {
-      case 'polo':
-        data = {
-          name: 'Marco',
-          surname: 'Polo',
-          dates: '1254 - 1324'
-        };
-        break;
-      case 'colombo':
-        data = {
-          name: 'Cristoforo',
-          surname: 'Colombo',
-          dates: '1451 - 1506'
-        };
-        break;
-      case 'vespucci':
-        data = {
-          name: 'Amerigo',
-          surname: 'Vespucci',
-          dates: '1454 - 1512'
-        };
-        break;
-      case 'da verrazzano':
-      case 'verrazzano':
-        data = {
-          name: 'Giovanni',
-          surname: 'da Verrazzano',
-          dates: '1485 - 1528'
-        };
-        break;
-      default:
-        data = {
-          name: 'unknown'
-        }
-    }
-  }
-  res.json(data);
-};
+// -----------------------------
+// Middleware за проверка
+// -----------------------------
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect('/');
+}
 
+// -----------------------------
+// 3. Свързване с базата данни
+// -----------------------------
+myDB(async (client) => {
+  const myDataBase = await client.db('database').collection('users');
 
-app.route('/travellers')
-  .put(travellers);
+  // Passport стратегия
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      if (password !== user.password) return done(null, false);
+      return done(null, user);
+    });
+  }));
 
-let error;
-app.get('/_api/get-tests', cors(), function (req, res, next) {
-  if (error)
-    return res.json({ status: 'unavailable' });
-  next();
-},
-  function (req, res, next) {
-    if (!runner.report) return next();
-    res.json(testFilter(runner.report, req.query.type, req.query.n));
-  },
-  function (req, res) {
-    runner.on('done', function (report) {
-      process.nextTick(() => res.json(testFilter(runner.report, req.query.type, req.query.n)));
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectId(id) }, (err, doc) => {
+      done(null, doc);
     });
   });
 
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log("Listening on port " + port);
-  console.log('Running Tests...');
-  setTimeout(function () {
-    try {
-      runner.run();
-    } catch (e) {
-      error = e;
-      console.log('Tests are not valid:');
-      console.log(error);
+  // Login
+  app.post('/login',
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res) => {
+      res.redirect('/profile');
     }
-  }, 1500);
+  );
+
+  // Profile
+  app.get('/profile', ensureAuthenticated, (req, res) => {
+    res.render('profile', { username: req.user.username });
+  });
+
+  // Register
+  app.post('/register', (req, res, next) => {
+    myDataBase.findOne({ username: req.body.username }, (err, user) => {
+      if (err) return next(err);
+      if (user) return res.redirect('/');
+
+      myDataBase.insertOne({
+        username: req.body.username,
+        password: req.body.password
+      }, (err, doc) => {
+        if (err) return res.redirect('/');
+        next(null, { _id: doc.insertedId, ...req.body });
+      });
+    });
+  },
+  passport.authenticate('local', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/profile');
+  });
+
+  // Logout
+  app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  // 404
+  app.use((req, res) => {
+    res.status(404).type('text').send('Not Found');
+  });
+
+}).catch((e) => {
+  console.error(e);
+  app.route('/').get((req, res) => {
+    res.render('index', { title: 'Error', message: 'Unable to connect to database' });
+  });
 });
 
-
-module.exports = app; // for testing
-
-function testFilter(tests, type, n) {
-  let out;
-  switch (type) {
-    case 'unit':
-      out = tests.filter(t => t.context.match('Unit Tests'));
-      break;
-    case 'functional':
-      out = tests.filter(t => t.context.match('Functional Tests'));
-      break;
-    default:
-      out = tests;
-  }
-  if (n !== undefined) {
-    return out[n] || out;
-  }
-  return out;
-}
+// -----------------------------
+// Start server
+// -----------------------------
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Listening on port ${PORT}`);
+});
