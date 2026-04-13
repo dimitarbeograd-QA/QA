f95bf2a76e0d9446139dbfe7c179c809b6796834
dimitarbeograd-QA <dmar91922@gmail.com>
2026-04-13 09:57:40 +0000

    fix

---
+++ connection.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const URI = process.env.MONGO_URI;
+
+module.exports = async function (callback) {
+  const client = new MongoClient(URI, { useUnifiedTopology: true });
+
+  try {
+    await client.connect();
+    console.log('Connected to database');
+    return callback(client);
+  } catch (e) {
+    console.error(e);
+    return callback(null, e);
+  }
+};
